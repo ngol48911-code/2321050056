3553c514e96abc9454b27ce3a5b7cdf938f378db
ngol48911-code <ngol48911@gmail.com>
2025-11-14 11:46:39 +0700

    up

--- bai71/scrip.js
+++ bai71/scrip.js
@@ -1,28 +1,66 @@
-let danhSachPhim = [
-  {
-    id: 1,
-    ten: " mua do ",
-    namPhatHanh: 2025,
-    tuoi: 16,
-    quocGia: "Viet Nam",
-    poster: "./pic/phosonhai.jpg",
-    theLoai: "chieu rap",
-  },
-  {
-    id: 2,
-    ten: "conan",
-    namPhatHanh: 2008,
-    tuoi: 16,
-    quocGia: "Nhat Ban",
-    poster: "./pic/conan.jpg",
-    theLoai: "phim hoat hinh   ",
-  },
-];
-let phimHienTai = danhSachPhim[0];
-let banner = document.getElementsByClassName("poster");
-function choPhim(idPhim) {
+function chonPhim(stt) {
+  let danhSachPhim = [
+    {
+      id: 1,
+      ten: " mua do ",
+      namPhatHanh: 2025,
+      tuoi: 16,
+      quocGia: "Viet Nam",
+      poster: "./pic/phosonhai.jpg",
+      theLoai: "chieu rap",
+    },
+    {
+      id: 2,
+      ten: "conan",
+      namPhatHanh: 2008,
+      tuoi: 16,
+      quocGia: "Nhat Ban",
+      poster: "./pic/conan.jpg",
+      theLoai: "phim hoat hinh ",
+    },
+    {
+      id: 3,
+      ten: "The Matrix",
+      namPhatHanh: 1999,
+      tuoi: 16,
+      quocGia: "USA",
+      poster: "./pic/matr.webp",
+      theLoai: "khoa hoc vien tuong",
+    },
+  ];
+
+  stt = parseInt(stt);
+  let ptChon = document.getElementById("poster");
+  let content = document.getElementById("content_poster");
+  let phim = null;
   for (let i = 0; i < danhSachPhim.length; i++) {
-    if ((danhSachPhim[i].backgroundImage = danhSachPhim[i].poster)) {
+    if (danhSachPhim[i].id === stt) {
+      phim = danhSachPhim[i];
+      break;
     }
   }
+  if (!phim) {
+    alert("khong co phim");
+  }
+  ptChon.setAttribute("src", phim.poster);
+  let noiDungHTML = `
+    <div class="film-content">
+      <h2 class="film-title">${phim.ten.trim()}</h2>
+      <div class="film-details">
+        <div class="film-info-row">
+          <p>${phim.namPhatHanh}</p>
+          <p>${phim.tuoi}+</p>
+          <p>${phim.quocGia}</p>
+          <p>${phim.theLoai.trim()}</p>
+        </div>
+      </div>
+      <div class="film-actions">
+        <button class="watch-btn">
+          <i class="fa-solid fa-play"></i>
+          Xem thêm
+        </button>
+      </div>
+    </div>
+  `;
+  content.innerHTML = noiDungHTML;
 }
---
+++ test.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS qly_dsphim;
+#1 the loai
+- id int PRIMARY KEY
+- ten_the_loai varchar(50)
+#2 phim 
+-id int PRIMARY KEY
+- dao_dien
+- daoDien
+- thoi_luong int 
+- paster varchar
+- mo_ta text 
+#3 nguoi dung 
+- id int 
+-tenDangNhap varchar(50)
+- matKhau varchar(50)
+- ho_ten varchar (50)
+-email varchar(50)
+-sdt varchar(10)
+- quyen_id int 
+-ngay_sinh datetime
+# quyen 
+- id_quyen varchar(20)
+-
+
+#4 quoc gia 
+-ten_quoc_gia varchar 
+- quoc_gia id
+#5 tap phim 
+- id
